2ee2a62291d8fec00f9cf1b9d379675d28f049a3
Ndcas <ndhung9911@gmail.com>
2025-05-12 03:05:06 +0700

    refactor

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const testRouter = require('./routers/test');
 const accountRouter = require('./routers/account');
 
+const port = process.env.APP_PORT;
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -16,8 +18,6 @@ app.use(express.json());
 app.use('/', testRouter);
 
 app.use('/account', accountRouter);
-
-let port = process.env.APP_PORT;
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
